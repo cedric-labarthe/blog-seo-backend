fcfee9227ba9185cfc3d5ce3672b9fb7de17d7ab
cedric-labarthe <cedric.labarth@gmail.com>
2022-06-06 21:39:21 +0200

    Added test + refacto

--- src/resolvers/article.spec.ts
+++ src/resolvers/article.spec.ts
@@ -1,3 +1,4 @@
+import { GraphQLError } from 'graphql/error/GraphQLError'
 import { DataSource } from 'typeorm'
 import { gqlCall } from '../configs/test-utils/gql-call'
 import { createTestConnection } from '../configs/test-utils/test-connection'
@@ -13,68 +14,102 @@ afterAll(async () => {
 describe('Article resolvers', () => {
   let createdId: string
 
-  it('should create and return article', async () => {
-    const createArticle = `
-    mutation Mutation($input: ArticleInput!) {
-      createArticle(input: $input) {
-        id
-        short_description
-        title
-        text
-        createdAt
-        updatedAt
-        deletedAt
-      }
-    }`
+  describe('Create =>', () => {
+    it('should create and return article', async () => {
+      const createArticle = `
+        mutation Mutation($input: ArticleInput!) {
+          createArticle(input: $input) {
+            id
+            short_description
+            title
+            text
+            createdAt
+            updatedAt
+            deletedAt
+          }
+        }`
 
-    const response = await gqlCall({
-      source: createArticle,
-      variableValues: {
-        input: {
-          title: 'test title',
-          short_description: 'bla bla bla',
-          text: 'Un grand bla bla bla',
+      const response = await gqlCall({
+        source: createArticle,
+        variableValues: {
+          input: {
+            title: 'test title',
+            short_description: 'bla bla bla',
+            text: 'Un grand bla bla bla',
+          },
         },
-      },
-    })
+      })
 
-    const data = response?.data?.createArticle
-
-    expect(data).toHaveProperty('title', 'test title')
-    expect(data).toHaveProperty('short_description', 'bla bla bla')
-    expect(data).toHaveProperty('text', 'Un grand bla bla bla')
-    expect(data).toHaveProperty('createdAt')
-    createdId = data.id
-    expect(data).toHaveProperty('updatedAt')
-    expect(data).toHaveProperty('id', createdId)
-  }),
-    it('should return full list (no pagination yet)', async () => {
-      const getArticles = `
-      query Articles {
-        getArticles {
-          title
-          text
-          short_description
-          createdAt
-          updatedAt
-          id
-        }
-      }
-    `
-
-      const response = await gqlCall({ source: getArticles })
-
-      const data = response?.data?.getArticles[0]
+      const data = response?.data?.createArticle
 
       expect(data).toHaveProperty('title', 'test title')
       expect(data).toHaveProperty('short_description', 'bla bla bla')
       expect(data).toHaveProperty('text', 'Un grand bla bla bla')
       expect(data).toHaveProperty('createdAt')
+      createdId = data.id
       expect(data).toHaveProperty('updatedAt')
       expect(data).toHaveProperty('id', createdId)
     }),
-    it('should return an article by id', async () => {
-      const getArticle = `
+      it('should fail with incorrect arguments', async () => {
+        const createArticle = `
+        mutation Mutation($input: ArticleInput!) {
+          createArticle(input: $input) {
+            id
+            short_description
+            title
+            text
+            createdAt
+            updatedAt
+            deletedAt
+          }
+        }`
+
+        const response = await gqlCall({
+          source: createArticle,
+          variableValues: {
+            input: {
+              title: 'test',
+              short_description: 'bla',
+              text: 'bla',
+            },
+          },
+        })
+
+        const errors = response?.errors
+
+        expect(errors?.[0]).toBeInstanceOf(GraphQLError)
+
+        expect(response?.data).toBeNull()
+      })
+  }),
+    describe('Read =>', () => {
+      it('should return full list (no pagination yet)', async () => {
+        const getArticles = `
+          query Articles {
+            getArticles {
+              title
+              text
+              short_description
+              createdAt
+              updatedAt
+              id
+            }
+          }
+        `
+
+        const response = await gqlCall({ source: getArticles })
+
+        const data = response?.data?.getArticles[0]
+
+        expect(data).toHaveProperty('title', 'test title')
+        expect(data).toHaveProperty('short_description', 'bla bla bla')
+        expect(data).toHaveProperty('text', 'Un grand bla bla bla')
+        expect(data).toHaveProperty('createdAt')
+        expect(data).toHaveProperty('updatedAt')
+        expect(data).toHaveProperty('id', createdId)
+      }),
+        it('should return an article by id', async () => {
+          const getArticle = `
       query Query($getArticleId: Float!) {
         getArticle(id: $getArticleId) {
           id
@@ -87,24 +122,86 @@ describe('Article resolvers', () => {
         }
       }`
 
-      const response = await gqlCall({
-        source: getArticle,
-        variableValues: {
-          getArticleId: Number(createdId),
-        },
-      })
+          const response = await gqlCall({
+            source: getArticle,
+            variableValues: {
+              getArticleId: Number(createdId),
+            },
+          })
 
-      const data = response?.data?.getArticle
+          const data = response?.data?.getArticle
 
-      expect(data).toHaveProperty('title', 'test title')
-      expect(data).toHaveProperty('short_description', 'bla bla bla')
-      expect(data).toHaveProperty('text', 'Un grand bla bla bla')
-      expect(data).toHaveProperty('createdAt')
-      expect(data).toHaveProperty('updatedAt')
-      expect(data).toHaveProperty('id', createdId)
+          expect(data).toHaveProperty('title', 'test title')
+          expect(data).toHaveProperty('short_description', 'bla bla bla')
+          expect(data).toHaveProperty('text', 'Un grand bla bla bla')
+          expect(data).toHaveProperty('createdAt')
+          expect(data).toHaveProperty('updatedAt')
+          expect(data).toHaveProperty('id', createdId)
+        })
     }),
-    it('should soft delete an article by id', async () => {
-      const softDeleteArticle = `
+    describe('Update =>', () => {
+      describe('should update an article by id', () => {
+        const updateArticle = `
+        mutation UpdateArticle($input: ArticleInput!) {
+          updateArticle(input: $input) {
+            id
+            title
+            short_description
+            text
+            createdAt
+            updatedAt
+          }
+        }`
+        it('could update only one field', async () => {
+          const response = await gqlCall({
+            source: updateArticle,
+            variableValues: {
+              input: {
+                id: Number(createdId),
+                title: 'test title updated',
+              },
+            },
+          })
+
+          const data = response?.data?.updateArticle
+
+          expect(data).toHaveProperty('title', 'test title updated')
+          expect(data).toHaveProperty('short_description', 'bla bla bla')
+          expect(data).toHaveProperty('text', 'Un grand bla bla bla')
+          expect(data).toHaveProperty('createdAt')
+          expect(data).toHaveProperty('updatedAt')
+          expect(data).toHaveProperty('id', createdId)
+        }),
+          it('could update all fields', async () => {
+            const response = await gqlCall({
+              source: updateArticle,
+              variableValues: {
+                input: {
+                  id: Number(createdId),
+                  title: 'test title full update',
+                  short_description: 'test short_description full update',
+                  text: 'test text full update',
+                },
+              },
+            })
+
+            const data = response?.data?.updateArticle
+
+            expect(data).toHaveProperty('title', 'test title full update')
+            expect(data).toHaveProperty(
+              'short_description',
+              'test short_description full update'
+            )
+            expect(data).toHaveProperty('text', 'test text full update')
+            expect(data).toHaveProperty('createdAt')
+            expect(data).toHaveProperty('updatedAt')
+            expect(data).toHaveProperty('id', createdId)
+          })
+      })
+    }),
+    describe('Delete =>', () => {
+      it('should soft delete an article by id', async () => {
+        const softDeleteArticle = `
       mutation Mutation($softDeleteArticleId: Float!) {
         softDeleteArticle(id: $softDeleteArticleId) {
           id
@@ -117,21 +214,28 @@ describe('Article resolvers', () => {
         }
       }`
 
-      const response = await gqlCall({
-        source: softDeleteArticle,
-        variableValues: {
-          softDeleteArticleId: Number(createdId),
-        },
+        const response = await gqlCall({
+          source: softDeleteArticle,
+          variableValues: {
+            softDeleteArticleId: Number(createdId),
+          },
+        })
+
+        const data = response?.data?.softDeleteArticle
+
+        expect(data).toHaveProperty('title', 'test title full update')
+        expect(data).toHaveProperty(
+          'short_description',
+          'test short_description full update'
+        )
+        expect(data).toHaveProperty('text', 'test text full update')
+        expect(data).toHaveProperty('createdAt')
+        expect(data).toHaveProperty('updatedAt')
+        expect(data).toHaveProperty('id', createdId)
+
+        expect(data).toHaveProperty('deletedAt')
+        expect(data.deletedAt).not.toBeNull()
+        expect(typeof data.deletedAt).toBe('string')
       })
-
-      const data = response?.data?.softDeleteArticle
-
-      expect(data).toHaveProperty('title', 'test title')
-      expect(data).toHaveProperty('short_description', 'bla bla bla')
-      expect(data).toHaveProperty('text', 'Un grand bla bla bla')
-      expect(data).toHaveProperty('createdAt')
-      expect(data).toHaveProperty('updatedAt')
-      expect(data).toHaveProperty('id', createdId)
-      expect(data).toHaveProperty('deletedAt')
     })
 })
